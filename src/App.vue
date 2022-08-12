e101468bceb3611aff29112594d7058bcf01cf15
Dimas Capelari <dimas.capelari@gmail.com>
2022-08-11 21:12:03 -0300

    aula 178

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>Registrar Reclamação</h1>
     <div class="conteudo">
-      <form class="painel">
+      <form class="painel" v-if="!enviado">
         <div class="cabecalho">Formulário</div>
         <Rotulo nome="E-mail">
           <input type="text" v-model.lazy.trim="usuario.email" />
@@ -62,9 +62,9 @@
           <Escolha v-model="escolha" />
         </Rotulo>
         <hr />
-        <button>Enviar</button>
+        <button @click.prevent="enviar">Enviar</button>
       </form>
-      <div class="painel">
+      <div class="painel" v-else>
         <div class="cabecalho">Resultado</div>
         <Rotulo nome="E-mail">
           <span>{{ usuario.email }}</span>
@@ -114,6 +114,11 @@ export default {
       return typeof this.prioridade;
     },
   },
+  methods: {
+    enviar() {
+      this.enviado = true;
+    },
+  },
   data() {
     return {
       mensagem: "",
@@ -131,6 +136,7 @@ export default {
         idade: 25,
       },
       escolha: true,
+      enviado: false,
     };
   },
   //   created() {
